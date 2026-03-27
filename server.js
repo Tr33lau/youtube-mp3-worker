0190a84c99527ee249744d92e11493f46ac31f02
Tr33lau <77994857+Tr33lau@users.noreply.github.com>
2026-03-27 00:23:45 -0500

    Create server.js

---
+++ server.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Worker is running");
+});
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server listening on port ${PORT}`);
+});
